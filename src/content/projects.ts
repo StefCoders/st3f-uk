79d4404afa37e165b061716cde67719dcd28d6fb
Stefan B <64033272+StefCoders@users.noreply.github.com>
2026-04-02 23:22:25 +0100

    Update projects.ts

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -10,18 +10,18 @@ export const projects: Project[] = [
 		name: "Moinesti FM",
 		description: "Romanian radio station I run, streaming live 24/7.",
 		url: "https://moinestifm.ro",
-		image: "/projects/moinestifm.png"
+		image: "https://moinestifm.ro/images/banner.png"
 	},
 	{
 		name: "London Romanian Radio",
 		description: "A station for the Romanian community in London.",
 		url: "https://lrradio.uk",
-		image: "/projects/lrradio.png"
+		image: "https://lrradio.uk/img/banner.png"
 	},
 	{
 		name: "st3f.uk blog",
 		description: "My personal site for projects, updates, and experiments.",
 		url: "/blog",
-		image: "/projects/st3fuk.png"
+		image: "/projects/stef.png"
 	}
 ];
